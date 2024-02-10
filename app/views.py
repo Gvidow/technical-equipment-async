610ab79e8e62d5958efb625eac6f614c599611bc
Gvidow <arzamachev.art-alex@yandex.ru>
2024-02-10 17:02:39 +0300

    async-service add: interaction with main backend

--- app/views.py
+++ app/views.py
@@ -6,17 +6,22 @@ import time
 import random
 import requests
 
+import os
+from dotenv import load_dotenv
+
 from concurrent import futures
 
-CALLBACK_URL = "http://0.0.0.0:8080/stocks/"
+CALLBACK_URL = "http://0.0.0.0:8080/api/v1/stocks/edit/"
+ACCESS_TOKEN_KEY = "TE_INTERNAL_ACCESS_TOKEN"
+load_dotenv()
 
 executor = futures.ThreadPoolExecutor(max_workers=1)
 
-def get_random_status(pk):
+def get_random_status(req_id):
     time.sleep(5)
     return {
-      "id": pk,
-      "status": bool(random.getrandbits(1)),
+      "id": req_id,
+      "status": random.uniform(0, 1) < 0.8,
     }
 
 def status_callback(task):
@@ -26,15 +31,15 @@ def status_callback(task):
     except futures._base.CancelledError:
       return
     
-    nurl = str(CALLBACK_URL+str(result["id"])+'/put/')
-    answer = {"is_growing": result["status"]}
-    requests.put(nurl, data=answer, timeout=3)
+    nurl = str(CALLBACK_URL+str(result["id"]))
+    access_token = os.environ.get(ACCESS_TOKEN_KEY, "")
+    answer = {"is_reverted": result["status"], "access_token": access_token}
+    requests.put(nurl, json=answer, timeout=3)
 
 @api_view(['POST'])
 def set_status(request):
-    if "pk" in request.data.keys():   
-        id = request.data["pk"]        
-
+    if "request_id" in request.data.keys():   
+        id = request.data["request_id"]        
         task = executor.submit(get_random_status, id)
         task.add_done_callback(status_callback)        
         return Response(status=status.HTTP_200_OK)
